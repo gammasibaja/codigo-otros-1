89ccd5c19a5fd7983a56f0ad569b807c86f0d6cc
Gamaliel Sibaja <gamsibaja@gmail.com>
2023-01-20 15:15:19 -0600

    se arreglaron distintos errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.getElementById('name');
+const $b = document.getElementById('blog');
+const $l = document.getElementById('location');
 
-function displayUser(username) {
+
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  
+  console.log(response);
+  $n.textContent = `${response.name}`;
+  $b.textContent = `${response.blog}`;
+  $l.textContent = `${response.location}`;
+  
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
